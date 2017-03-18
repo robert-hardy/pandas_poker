1c1b35fba40d25a40c2f2009d16e2cd4f27680cb
Robert Hardy <robbert@gmail.com>
2017-03-18 09:00:47 +0000

    List the winner and print simple stats.

--- run.py
+++ run.py
@@ -19,3 +19,18 @@ df['class1'] = df['score1'].apply(lambda x: evaluator.get_rank_class(x))
 df['class2'] = df['score2'].apply(lambda x: evaluator.get_rank_class(x))
 df['class1_string'] = df['class1'].apply(lambda x: evaluator.class_to_string(x))
 df['class2_string'] = df['class2'].apply(lambda x: evaluator.class_to_string(x))
+
+def who_won(x, y):
+    if x == y:
+        return 0
+    if x > y:
+        return 1
+    return 2
+
+df['winner'] = df.apply(lambda x: who_won(x['score1'], x['score2']), axis=1)
+
+
+# Was the game fair?
+draws, one, two = df.groupby('winner').count().iloc[:, 0].values
+
+print "Draws: {0}\nPlayer one: {1}\nPlayer two: {2}\n".format(draws, one, two)
