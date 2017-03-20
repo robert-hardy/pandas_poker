319ba7234136f586be271d5a590c4a32123b1f45
Robert Hardy <robbert@gmail.com>
2017-03-20 07:06:04 +0000

    Start to refactor, dealing first.

--- run.py
+++ run.py
@@ -9,14 +9,17 @@ import random
 random.seed(0)
 asserts = False
 
-all_cards = [ Deck().draw(9) for x in range(1000) ]
-df = pd.DataFrame({'all': all_cards})
+def deal_cards(nb_players=2):
+    all_cards = [ Deck().draw(9) for x in range(1000) ]
+    df = pd.DataFrame({'all': all_cards})
 
-df['board'] = df['all'].apply(lambda x: x[:5])
-df['player1'] = df['all'].apply(lambda x: x[5:7])
-df['player2'] = df['all'].apply(lambda x: x[7:9])
-del df['all']
+    df['board'] = df['all'].apply(lambda x: x[:5])
+    df['player1'] = df['all'].apply(lambda x: x[5:7])
+    df['player2'] = df['all'].apply(lambda x: x[7:9])
+    del df['all']
+    return df
 
+df = deal_cards()
 evaluator = Evaluator()
 df['score1'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player1']), axis=1)
 df['score2'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player2']), axis=1)
