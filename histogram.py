06dcb366ff89be06b37014f0d6edfce18d09a633
Robert Hardy <robbert@gmail.com>
2017-04-01 23:51:41 +0000

    Make a plot.
    Works.

--- histogram.py
+++ histogram.py
@@ -11,7 +11,7 @@ import random
 
 random.seed(0)
 def deal_cards(nb_players=2):
-    all_cards = [ Deck().draw(5 + 2*nb_players) for x in range(10000) ]
+    all_cards = [ Deck().draw(5 + 2*nb_players) for x in range(1000) ]
     df = pd.DataFrame({'all': all_cards})
     df['board'] = df['all'].apply(lambda x: x[:5])
     for i in range(nb_players):
@@ -26,12 +26,10 @@ evaluator = Evaluator()
 df['score1'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player1']), axis=1)
 df['score2'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player2']), axis=1)
 
-all_scores = [ s for l in df[['score1', 'score2']].values.tolist() for s in l ]
-c = Counter([ round(x/100)*100 for x in all_scores ])
-foo = [ (k, v, evaluator.class_to_string(evaluator.get_rank_class(k)))
-    for (k, v) in c.most_common(20) ]
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
-bar = [ evaluator.get_rank_class(s) for s in all_scores ]
-quux = [ (x, evaluator.class_to_string(x)) for x in bar ]
-string_counter = Counter(quux)
-string_counter.most_common(10)
+ax = df.plot()
+fig = ax.get_figure()
+fig.savefig('foo.png')
