da9c9a8de9e70d9b64b457773c16dfe86894c7c6
Robert Hardy <robbert@gmail.com>
2017-04-01 22:50:41 +0000

    Script to create simple count of rankings.
    No df, just lists.
    But it gets us started well.

---
+++ initial_rank_histogram.py
@@ -0,0 +1,13 @@
+from collections import Counter
+from deuces import (
+    Deck,
+    Card,
+    Evaluator
+)
+
+all_cards = [ Deck().draw(5) for x in range(10000) ]
+vals = [ evaluator.evaluate(x, []) for x in all_cards ]
+ranks = [ ( evaluator.get_rank_class(v),
+    evaluator.class_to_string(evaluator.get_rank_class(v)) ) for v in vals ]
+c = Counter(ranks)
+c.most_common(10)
