da834c70a2533fdffdbc72bbea83897a3b0f8bed
Robert Hardy <robbert@gmail.com>
2017-04-02 10:52:29 +0000

    Deck itself is a generator.
    All green.

--- tests/test_dataframe_construction.py
+++ tests/test_dataframe_construction.py
@@ -9,16 +9,10 @@ random.seed(0)
 
 
 class TestDFConstruction(unittest.TestCase):
-    def setUp(self):
-        self.all_cards = [ Deck().draw(5 + 2*2) for x in range(5) ]
-
     def test_board(self):
-        hand = [ (i for i in hand) for hand in self.all_cards ]
+        hand = [ Deck() for _ in range(5) ]
         df_board = pd.DataFrame({
-            'board': [
-                [cards.next() for _ in range(5)]
-                for cards in hand
-            ]
+            'board': [ deck.draw(5) for deck in hand ]
         })
         expected = [
             [[81922, 33560861, 139523, 16783383, 147715]],
