cf2fe0cc30f1ffcece14a1efaf43fbd1c6dc300b
Robert Hardy <robbert@gmail.com>
2017-04-02 12:18:55 +0000

    Update the dataframe constructor with the new tricks.
    All working well.

--- dataframe_builder.py
+++ dataframe_builder.py
@@ -1,14 +1,15 @@
 from deuces import (
     Deck,
-    Card,
     Evaluator
 )
 import pandas as pd
 import random
 
+NB_HANDS = 10000
+NB_PLAYERS = 3
 
 random.seed(0)
-hand = [ Deck() for _ in range(5) ]
+hand = [ Deck() for _ in range(NB_HANDS) ]
 df_board = pd.DataFrame({
     'board': [ deck.draw(5) for deck in hand ]
 })
@@ -17,7 +18,17 @@ players = [
     pd.DataFrame({
         'player': [ deck.draw(2) for deck in hand ]
     })
-    for _ in range(2)
+    for _ in range(NB_PLAYERS)
 ]
-df_players = pd.concat(players, axis=1, keys=range(2))
+df_players = pd.concat(players, axis=1, keys=range(NB_PLAYERS))
 df_players = df_players.swaplevel(0, 1, axis=1)
+
+evaluator = Evaluator()
+df_scores = pd.concat([
+    df_board.join(df_players.xs(i, level=1, axis=1)).apply(
+        lambda x: evaluator.evaluate(x['board'], x['player']),
+        axis=1
+    ).to_frame('score')
+    for i in df_players.columns.get_level_values(1)
+], axis=1, keys=range(NB_PLAYERS))
+df_scores = df_scores.swaplevel(0, 1, axis=1)
