5db9f3cf10346911f66c4a62725110031d6aba8d
Robert Hardy <robbert@gmail.com>
2017-04-02 11:02:29 +0000

    Remove test on random seed, replace with test that draws line up.
    All green.

--- tests/test_dataframe_construction.py
+++ tests/test_dataframe_construction.py
@@ -26,7 +26,24 @@ class TestDFConstruction(unittest.TestCase):
             expected
         )
 
-    def test_random_seed_resets_as_expected(self):
+    def test_draws_are_consistent(self):
+        random.seed(0)
+        hand = [ Deck() for _ in range(5) ]
+        df_all = pd.DataFrame({
+            'all': [ deck.draw(9) for deck in hand ]
+        })
+        expected = [
+            [[81922, 33560861, 139523, 16783383, 147715, 69634, 164099, 1053707, 16795671]],
+            [[16783383, 4212241, 295429, 69634, 81922, 73730, 134236965, 8394515, 557831]],
+            [[266757, 1082379, 134236965, 557831, 16787479, 67127839, 529159, 134253349, 1053707]],
+            [[33564957, 8423187, 1065995, 33589533, 98306, 268471337, 2114829, 16812055, 67119647]],
+            [[16795671, 529159, 135427, 16783383, 266757, 2106637, 279045, 8423187, 8394515]]
+        ]
+        self.assertEqual(
+            df_all.values.tolist(),
+            expected
+        )
+
         random.seed(0)
         hand = [ Deck() for _ in range(5) ]
         df_board = pd.DataFrame({
