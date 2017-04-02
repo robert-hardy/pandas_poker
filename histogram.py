df1b2faf1fe2ce4fb89eaa5ae9dd1b2f8649de62
Robert Hardy <robbert@gmail.com>
2017-04-02 00:14:30 +0000

    Simulate 3 hands and add points to the scatter.
    Lots is happening, perhaps better to focus on the worse-valued hands?

--- histogram.py
+++ histogram.py
@@ -11,7 +11,7 @@ import random
 
 random.seed(0)
 def deal_cards(nb_players=2):
-    all_cards = [ Deck().draw(5 + 2*nb_players) for x in range(1000) ]
+    all_cards = [ Deck().draw(5 + 2*nb_players) for x in range(10000) ]
     df = pd.DataFrame({'all': all_cards})
     df['board'] = df['all'].apply(lambda x: x[:5])
     for i in range(nb_players):
@@ -21,15 +21,19 @@ def deal_cards(nb_players=2):
     del df['all']
     return df
 
-df = deal_cards()
+df = deal_cards(3)
 evaluator = Evaluator()
 df['score1'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player1']), axis=1)
 df['score2'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player2']), axis=1)
+df['score3'] = df.apply(lambda x: evaluator.evaluate(x['board'], x['player3']), axis=1)
+
+df['score_min23'] = df[['score2', 'score3']].min(axis=1)
 
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 ax = df.plot.scatter(x='score1', y='score2')
+df.plot.scatter(x='score1', y='score_min23', color='Red', marker='+', ax=ax)
 fig = ax.get_figure()
 fig.savefig('foo.png')
