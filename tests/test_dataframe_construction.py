4b652b2366d7baaaca1406b8e48940ffc0bb0268
Robert Hardy <robbert@gmail.com>
2017-04-02 10:40:11 +0000

    Start building out tests.
    All green (1 test).

---
+++ tests/test_dataframe_construction.py
@@ -0,0 +1,23 @@
+import unittest
+
+from deuces import (
+    Deck
+)
+import pandas as pd
+import random
+random.seed(0)
+
+
+class TestDFConstruction(unittest.TestCase):
+    def setUp(self):
+        self.all_cards = [ Deck().draw(5 + 2*2) for x in range(5) ]
+
+    def test_board(self):
+        hand = [ (i for i in hand) for hand in self.all_cards ]
+        df_board = pd.DataFrame({
+            'board': [
+                [cards.next() for _ in range(5)]
+                for cards in hand
+            ]
+        })
+        self.assertEqual(len(df_board), len(self.all_cards))
