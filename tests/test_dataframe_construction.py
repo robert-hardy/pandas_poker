ad792e3c887723a59940602a6d43112fcecdabd3
Robert Hardy <robbert@gmail.com>
2017-04-02 10:57:59 +0000

    Proof that resetting the seed works.
    All green.

--- tests/test_dataframe_construction.py
+++ tests/test_dataframe_construction.py
@@ -25,3 +25,21 @@ class TestDFConstruction(unittest.TestCase):
             df_board.values.tolist(),
             expected
         )
+
+    def test_random_seed_resets_as_expected(self):
+        random.seed(0)
+        hand = [ Deck() for _ in range(5) ]
+        df_board = pd.DataFrame({
+            'board': [ deck.draw(5) for deck in hand ]
+        })
+        expected = [
+            [[81922, 33560861, 139523, 16783383, 147715]],
+            [[16783383, 4212241, 295429, 69634, 81922]],
+            [[266757, 1082379, 134236965, 557831, 16787479]],
+            [[33564957, 8423187, 1065995, 33589533, 98306]],
+            [[16795671, 529159, 135427, 16783383, 266757]]
+        ]
+        self.assertEqual(
+            df_board.values.tolist(),
+            expected
+        )
