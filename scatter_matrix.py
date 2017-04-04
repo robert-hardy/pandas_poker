227a17a8f1c52fcca34953b4311e6fda8999df98
Robert Hardy <robbert@gmail.com>
2017-04-04 22:31:19 +0000

    5 players, 500 hands.

--- scatter_matrix.py
+++ scatter_matrix.py
@@ -4,7 +4,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 
-_, _, df_scores = build_dataframe()
+_, _, df_scores = build_dataframe(
+    nb_hands=500,
+    nb_players=5
+)
 
 ax = pd.scatter_matrix(df_scores)
 plt.savefig('scatter_matrix.png')
