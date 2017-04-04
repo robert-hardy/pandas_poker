a290d7aeed9d26b08413c50972a96e6fd7454b60
Robert Hardy <robbert@gmail.com>
2017-04-04 22:41:39 +0000

    Set the axis ranges in the scatters.

--- scatter_matrix.py
+++ scatter_matrix.py
@@ -10,4 +10,10 @@ _, _, df_scores = build_dataframe(
 )
 
 ax = pd.scatter_matrix(df_scores, s=2)
+
+for i, row in enumerate(ax):
+    for j, item in enumerate(row):
+        if i != j:
+            item.set_xlim(0, 8000)
+            item.set_ylim(0, 8000)
 plt.savefig('scatter_matrix.png')
