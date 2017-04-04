0b2b0b400b7a17f99b55ff843ddf050780b684ec
Robert Hardy <robbert@gmail.com>
2017-04-04 23:06:46 +0000

    Calculate min score of all opponents.
    Shows impact of playing in hands with more opponents.

--- scatter_matrix.py
+++ scatter_matrix.py
@@ -13,6 +13,20 @@ _, _, df_scores = build_dataframe(
 
 df_scores.columns = df_scores.columns.get_level_values(1)
 
+df_opponents = pd.concat(
+    [
+        df_scores[[0]],
+        df_scores[[1]].min(axis=1),
+        df_scores[[1, 2]].min(axis=1),
+        df_scores[[1, 2, 3]].min(axis=1),
+        df_scores[[1, 2, 3, 4]].min(axis=1)
+    ],
+    axis = 1,
+    keys = range(5)
+)
+df_opponents.columns = df_opponents.columns.get_level_values(0)
+
+
 if __name__ == '__main__':
     ax = pd.scatter_matrix(df_scores, s=2)
     for i, row in enumerate(ax):
@@ -21,3 +35,11 @@ if __name__ == '__main__':
                 item.set_xlim(0, 8000)
                 item.set_ylim(0, 8000)
     plt.savefig('scatter_matrix.png')
+
+    ax = pd.scatter_matrix(df_opponents, s=2)
+    for i, row in enumerate(ax):
+        for j, item in enumerate(row):
+            if i != j:
+                item.set_xlim(0, 8000)
+                item.set_ylim(0, 8000)
+    plt.savefig('scatter_matrix_opponents.png')
