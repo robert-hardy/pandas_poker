e8126e1182d8b6508edea903732ec078d8880ef7
Robert Hardy <robbert@gmail.com>
2017-04-05 00:42:15 +0000

    Run the PnL chart against net result of opponents.

--- pnl_chart.py
+++ pnl_chart.py
@@ -3,12 +3,26 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import random
+import pandas as pd
 
 
 random.seed(0)
-_, _, df = build_dataframe()
+_, _, df_scores = build_dataframe(nb_players=5)
+df_scores.columns = df_scores.columns.get_level_values(1)
 
-df.columns = df.columns.get_level_values(1)
+df = pd.concat(
+    [
+        df_scores[[0]],
+        df_scores[[1]].min(axis=1),
+        df_scores[[1, 2]].min(axis=1),
+        df_scores[[1, 2, 3]].min(axis=1),
+        df_scores[[1, 2, 3, 4]].min(axis=1)
+    ],
+    axis = 1,
+    keys = range(5)
+)
+df.columns = df.columns.get_level_values(0)
+
 df['win'] = df[1] - df[0]
 
 df.ix[df.win!=0, 'win'] = df.ix[df.win!=0, 'win'] / df.ix[df.win!=0, 'win'].abs()
