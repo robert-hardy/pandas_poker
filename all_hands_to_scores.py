bfcf7bf2d8d5fcf8c6a42f34264ce35647ae43ef
Robert Hardy <robbert@gmail.com>
2017-04-07 22:47:10 +0000

    Add score_to_pc function and minor fixes.

--- all_hands_to_scores.py
+++ all_hands_to_scores.py
@@ -20,7 +20,10 @@ df = s.to_frame('score')
 
 LENGTH = len(df)
 df['pc'] = (1.0 + df.index)/LENGTH
-df = df.remove_duplicates('score', take_last=True)
+df = df.drop_duplicates('score', keep='last')
 df = df.set_index('score')
 df.plot()
 plt.savefig('score_to_pc.png')
+
+def score_to_pc(score):
+    return df.ix[score, 0]
