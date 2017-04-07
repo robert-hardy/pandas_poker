deb53f11b65969ed7bd361b958ead154af24532a
Robert Hardy <robbert@gmail.com>
2017-04-07 21:10:46 +0000

    Script to calculate the table of scores and rank counts.

---
+++ rank_score_probability_table.py
@@ -0,0 +1,19 @@
+from deuces import (
+    Deck,
+    Evaluator
+)
+from itertools import combinations
+import pandas as pd
+
+
+deck = Deck().draw(52)
+e = Evaluator()
+all_hands = [ list(x) for x in combinations(deck, 5) ]
+all_scores = [ e.evaluate(x, []) for x in all_hands ]
+all_ranks = [ e.get_rank_class(x) for x in all_scores ]
+
+df_all = pd.DataFrame({
+    'score': all_scores,
+    'rank': all_ranks
+})
+df = df_all.groupby('rank').agg({'rank': 'count', 'score': [min, max]})
