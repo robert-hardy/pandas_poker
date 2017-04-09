8334d585eef2a7821984f2eda576bf9a4ad9d424
Robert Hardy <robbert@gmail.com>
2017-04-09 16:04:57 +0000

    Manually add 4 players into the build function.

--- app/build_dataframe.py
+++ app/build_dataframe.py
@@ -10,11 +10,24 @@ def build_dataframe(nb_hands):
     boards = [ deck.draw(5) for deck in decks ]
     player1 = [ deck.draw(2) for deck in decks ]
     player2 = [ deck.draw(2) for deck in decks ]
+    player3 = [ deck.draw(2) for deck in decks ]
+    player4 = [ deck.draw(2) for deck in decks ]
 
     e = Evaluator()
     scores = [
-        (e.evaluate(b, p1), e.evaluate(b, p2))
-        for (b, p1, p2) in zip(boards, player1, player2)
+        (
+            e.evaluate(b, p1),
+            e.evaluate(b, p2),
+            e.evaluate(b, p3),
+            e.evaluate(b, p4)
+        )
+        for (b, p1, p2, p3, p4) in zip(
+            boards,
+            player1,
+            player2,
+            player3,
+            player4
+        )
     ]
 
     df = pd.DataFrame(scores)
