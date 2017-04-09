890ceda9921f44e54758da62c1be77d3aa6237a7
Robert Hardy <robbert@gmail.com>
2017-04-09 15:07:10 +0000

    Generate 2-col df of scores.
    In the easiest possible way: lists -> df.

---
+++ app/generate_scatter_matrix_1.py
@@ -0,0 +1,18 @@
+from deuces import Deck, Evaluator
+import pandas as pd
+import random
+random.seed(0)
+
+
+decks = [ Deck() for _ in range(5) ]
+boards = [ deck.draw(5) for deck in decks ]
+player1 = [ deck.draw(2) for deck in decks ]
+player2 = [ deck.draw(2) for deck in decks ]
+
+e = Evaluator()
+scores = [
+    (e.evaluate(b, p1), e.evaluate(b, p2))
+    for (b, p1, p2) in zip(boards, player1, player2)
+]
+
+df = pd.DataFrame(scores)
