8615e50c6d02de7410f0ed5bb669288d8d571725
Robert Hardy <robbert@gmail.com>
2017-04-09 14:35:08 +0000

    Better organized use of the Counter.

--- initial_rank_histogram.py
+++ initial_rank_histogram.py
@@ -3,10 +3,17 @@ from deuces import (
     Deck,
     Evaluator
 )
+import random
+random.seed(1)
 
-all_cards = [ Deck().draw(5) for x in range(10000) ]
-vals = [ evaluator.evaluate(x, []) for x in all_cards ]
-ranks = [ ( evaluator.get_rank_class(v),
-    evaluator.class_to_string(evaluator.get_rank_class(v)) ) for v in vals ]
-c = Counter(ranks)
-c.most_common(10)
+
+hands = [ Deck().draw(5) for _ in range(10000) ]
+
+e = Evaluator()
+scores = [ e.evaluate(x, []) for x in hands ]
+ranks = [ e.get_rank_class(s) for s in scores ]
+rank_strings = [ e.class_to_string(r) for r in ranks ]
+
+c = Counter(rank_strings)
+for i in c.most_common(10):
+    print i
