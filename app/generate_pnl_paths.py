b7316ddc5512b37440d8ed9100ef1dd6c02039db
Robert Hardy <robbert@gmail.com>
2017-04-09 15:39:02 +0000

    Generate and save the plot of the pnl path.

--- app/generate_pnl_paths.py
+++ app/generate_pnl_paths.py
@@ -1,7 +1,13 @@
 from app.build_dataframe import build_dataframe
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 df = build_dataframe(1000)
 
 pnl = df[1] - df[0]
 pnl.ix[pnl != 0] = pnl.ix[pnl != 0] / abs(pnl)
 pnl = pnl.cumsum()
+
+pnl.plot()
+plt.savefig('plots/pnl_plot_1.png')
