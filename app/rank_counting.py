1c5bc5d6617fa221743f280989242800e8f09061
Robert Hardy <robbert@gmail.com>
2017-04-09 14:48:49 +0000

    Show times as the lists are being built and processed.

--- app/rank_counting.py
+++ app/rank_counting.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from datetime import datetime
 from deuces import (
     Deck,
     Evaluator
@@ -7,13 +8,19 @@ import random
 random.seed(1)
 
 
-hands = [ Deck().draw(5) for _ in range(10000) ]
+print 'Generating the hands.'
+print datetime.now()
+hands = [ Deck().draw(5) for _ in range(1000000) ]
 
 e = Evaluator()
+print 'Scoring them.'
+print datetime.now()
 scores = [ e.evaluate(x, []) for x in hands ]
 ranks = [ e.get_rank_class(s) for s in scores ]
 rank_strings = [ e.class_to_string(r) for r in ranks ]
 
+print 'Counting them.'
+print datetime.now()
 c = Counter(rank_strings)
 for i in c.most_common(10):
     print i
